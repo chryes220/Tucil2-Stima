29da69c625571f3b9d4a7aa679614a49d2749e8b
chryes220 <christinehutabarat12@gmail.com>
2022-02-26 12:55:11 +0700

    fix convex hull algorithm, add visualize function

--- src/ft.py
+++ src/ft.py
@@ -1,13 +1,32 @@
 # ft.py
 
-from myConvexHull import myConvexHull
-
-#create a DataFrame 
-import numpy as np
-list = [[1,1], [2,2], [4,4], [0,0], [1,2], [3,1], [3,3]]
-
-ch = myConvexHull(list)
-print(ch.points)
+from sklearn import datasets
+from myConvexHull import myConvexHull 
+import matplotlib.pyplot as plt
+data = datasets.load_iris()
+data.data = data.data[:5,:2]
+'''
+print(data.data)
+ch = myConvexHull(data.data.tolist())
 print(ch.vertices)
-print(ch.lines)
+print(ch.points)
 print(ch.contained)
+print(ch.lines)
+'''
+
+data2 = [[0,1], [2,4], [4,4], [0,0], [1,1], [3,1], [3,2]]
+'''
+ch2 = myConvexHull(data2)
+print(ch2.vertices)
+print(ch2.points)
+print(ch2.contained)
+print(ch2.lines)
+ch2.visualize() '''
+
+data3 = [[5.1,3.5], [4.9,3.0], [4.7,3.2], [4.6,3.1], [5.0,3.6]] 
+
+ch3 = myConvexHull(data3)
+print(ch3.vertices)
+print(ch3.points)
+print(ch3.contained)
+print(ch3.lines)
--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -1,6 +1,8 @@
 # myConvexHull.py
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import math
 
 class myConvexHull:
@@ -8,17 +10,26 @@ class myConvexHull:
     vertices = [] # berisi indeks dari point dalam points yang membentuk convex hull
     lines = [] # berisi indeks dari point-point yang membentuk garis convex hull (didefinisikan sebagai array of integer dengan panjang 2 element)
     outer = [] # berisi indeks dari dua point terluar, dengan points[outer[0]][0] < points[outer[1]][0]
-    contained = [] # berisi indeks dari point-point yang sudah berada di dalam area
+    contained = set() # set yang berisi indeks dari point-point yang sudah berada di dalam area
+    data = []
+    df = []
+    count_contained = 0
 
-    def __init__(self, arr_of_points) :
-        # points dalam parameter bertipe array
-        self.points = arr_of_points
+    def __init__(self, data) :
+        # arr_of_points bertipe Bunch, namun diubah dan diolah dalam bentuk array of points
+        '''
+        self.data = data # Bunch masih disimpan
+        self.df = pd.DataFrame(data.data, columns=data.feature_names) # juga disimpan dalam bentuk data frame
+        self.points = data.data[:,:2].tolist() '''
+        self.points = data
 
         self.outer = self.find_outer_points()
         # since outer points surely create the convex hull
         self.vertices.append(self.outer[0])
         self.vertices.append(self.outer[1])
         self.lines.append([self.outer[0], self.outer[1]])
+
+        self.count_contained = 2
 
         self.findConvexHull(self.points[self.outer[0]], self.points[self.outer[1]], self.points)
 
@@ -55,25 +66,31 @@ class myConvexHull:
         c = (point_line1[0] * point_line2[1]) - (point_line2[0] * point_line1[1])
 
         for point in region :
-            dist = abs(((a * point[0]) + (b * point[1]) + c)/math.sqrt(a**2 + b**2))
-            if (dist > furthest) :
-                furthest = dist
-                idx = self.points.index(point)
+            denom = math.sqrt(a**2 + b**2)
+            if (denom != 0) :
+                dist = abs(((a * point[0]) + (b * point[1]) + c)/denom)
+                if (dist > furthest) :
+                    furthest = dist
+                    idx = self.points.index(point)
         return idx
 
-    def update_contained(self, p1, p2, p3, region) :
-        # p3 adalah bagian dari region
+    def is_contained(self, p1, p2, p3, point) :
+        # menggunakan barycentric coordinate
         # referensi : https://mathworld.wolfram.com/TriangleInterior.html#:~:text=The%20simplest%20way%20to%20determine,it%20lies%20outside%20the%20triangle.
+        
+        denom = (p2[1] - p3[1])*(p1[0] - p3[0]) + (p3[0] - p2[0])*(p1[1] - p3[1])
+        if (denom != 0) :
+            a = ((p2[1] - p3[1])*(point[0] - p3[0]) + (p3[0] - p2[0])*(point[1] - p3[1])) / denom
+            b = ((p3[1] - p1[1])*(point[0] - p3[0]) + (p1[0] - p3[0])*(point[1] - p3[1])) / denom
+            c = 1 - a - b
+            return ((0 <= a <= 1) and (0 <= b <= 1) and (0 <= c <= 1))
+        else :
+            return False
+    
+    def update_contained(self, p1, p2, p3, region) :
         for point in region :
-            div = (p2[1] - p3[1])*(p1[0] - p3[0]) + (p3[0] - p2[0])*(p1[1] - p3[1])
-            if (div != 0) :
-                a = ((p2[1] - p3[1])*(point[0] - p3[0]) + (p3[0] - p2[0])*(point[1] - p3[1])) / div
-                b = ((p3[1] - p1[1])*(point[0] - p3[0]) + (p1[0] - p3[0])*(point[1] - p3[1])) / div
-                c = 1 - a - b
-
-                if (0 <= a <= 1 and 0 <= b <= 1 and 0 <= c <= 1) :
-                    # titik ada di dalam segitiga
-                    self.contained.append(self.points.index(point))
+            if (self.is_contained(p1, p2, p3, point)) :
+                self.contained.add(self.points.index(point))
 
     def findConvexHull (self, point_line1, point_line2, region) :
         # region : array of points
@@ -92,35 +109,65 @@ class myConvexHull:
                     regA.append(point)
                 elif (det < 0) :
                     regB.append(point)
-                # semua point yang berada pada garis diabaikan
+                else :
+                    self.contained.add(self.points.index(point))
 
-            #print("region A ", regA)
-            #print("region B", regB)
+            a_is_not_contained = (len(regA) > 0 and not self.points.index(regA[0]) in self.contained)
+            b_is_not_contained = (len(regB) > 0 and not self.points.index(regB[0]) in self.contained)
+
+            new_triangleA_created = False
+            new_triangleB_created = False
+
 
             # if any of the region is contained already, just skip
             # find furthest point in the not-contained area
             # then, update the vertices and lines
-            if (len(regA) > 0 and not (regA[0] in self.contained)) :
+            if (a_is_not_contained) :
                 furthestA = self.find_furthest_idx(point_line1, point_line2, regA)
-                #print("furthestA : ", furthestA)
                 self.vertices.append(furthestA)
                 self.lines.append([pl1_idx, furthestA])
                 self.lines.append([pl2_idx, furthestA])
+                # setelah terbentuk segitiga dalam region, update dulu point-point mana aja yang masuk ke segitiga
                 self.update_contained(point_line1, point_line2, self.points[furthestA], regA)
-                # next iteration
+
+                for point in regA :
+                    if (self.points.index(point) in self.contained) :
+                        regA.pop(regA.index(point))
+                new_triangleA_created = (furthestA != -1)
+                # bagi region menjadi yang lebih dekat dengan masing-masing
                 self.findConvexHull(point_line1, self.points[furthestA], regA)
                 self.findConvexHull(point_line2, self.points[furthestA], regA)
-            if (len(regB) > 0 and not (regB[0] in self.contained)) :
+            if (b_is_not_contained) :
                 furthestB = self.find_furthest_idx(point_line1, point_line2, regB)
-                #print("furthestB : ", furthestB)
                 self.vertices.append(furthestB)
                 self.lines.append([pl1_idx, furthestB])
                 self.lines.append([pl2_idx, furthestB])
                 self.update_contained(point_line1, point_line2, self.points[furthestB], regB)
-                # next iteration
+                
+                for point in regB :
+                    if (self.points.index(point) in self.contained) :
+                        regB.pop(regB.index(point))
+                new_triangleB_created = (furthestB != -1)
                 self.findConvexHull(point_line1, self.points[furthestB], regB)
                 self.findConvexHull(point_line2, self.points[furthestB], regB)
+
             
             # delete the line created by point_line1 and point_line2
-            self.lines.pop(self.lines.index([pl1_idx, pl2_idx]))
+            if(new_triangleA_created or new_triangleB_created) :
+                self.lines.pop(self.lines.index([pl1_idx, pl2_idx]))
+                print("popped lines : ", self.lines)
 
+    def visualize (self) :
+        # berhubung kita cuma punya points sama lines yang berbentuk array
+        plt.figure(figsize = (10, 6))
+        colors = ['b','r','g']
+        plt.title('Petal Width vs Petal Length')
+        plt.xlabel('sepal length')
+        plt.ylabel('sepal width')
+        # sudah bikin 'ruang' untuk plot, waktunya memplot
+        for point in self.points :
+            plt.plot(point[0], point[1], 'bo')
+        for line in self.lines :
+            plt.plot([self.points[line[0]][0], self.points[line[1]][0]], [self.points[line[0]][1], self.points[line[1]][1]], 'bo-')
+        plt.legend()
+        plt.show()
